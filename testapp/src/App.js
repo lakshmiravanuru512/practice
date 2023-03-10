728e0784055f1395bb91d9610498711a4d04f4e7
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-10 18:11:08 +0530

    functional component example

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,51 +1,5 @@
-import UserProfile from "./components";
-import "./App.css"
-const userDetailsList=[{
-  uniqueNo:1,
-  imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name:"Lakshmi",
-  role:"Frontened Developer"
-},
-{
-  uniqueNo:2,
-  imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name:"Lakshmi",
-  role:"Frontened Developer"
-}, 
-{
-  uniqueNo:3,
-  imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name:"Lakshmi",
-  role:"Frontened Developer"
-},
-{
-  uniqueNo:4,
-  imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name:"Lakshmi",
-  role:"Frontened Developer"
-},
-{
-  uniqueNo:5,
-  imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
-  name:"Lakshmi",
-  role:"Frontened Developer"
+import Welcome from  "./components";
+const App =()=>{
+  return <Welcome/>
 }
-
- ]
-
-
-const App=()=>(
-  <div className="list-container">
-    <h1 className="title">Users List</h1>
-    <ul>
-     {
-     userDetailsList.map((eachItem)=>(
-     <UserProfile userDetails={eachItem} key={eachItem.uniqueNo}/>
-     ))
-     }
-    </ul>
-  </div>
-)
-
-
 export default App;
--- testapp/src/components/index.js
+++ testapp/src/components/index.js
@@ -1,18 +1,3 @@
-import "./index.css" 
-const UserProfile=(props)=>{
-    
-    const {userDetails}=props;
-    const{imageUrl,name,role}=userDetails;
+const Welcome =()=><h1>Functional Component</h1>
 
-    return(
-    <div className="card-container">
-        <img src={imageUrl} className="avatar" alt="Avatar"/>
-        <div className="user-details-container">
-            <h1 className="user-name">{name}</h1>
-            <p className="user-role">{role}</p>
-
-        </div>
-    </div>
-)
-}
-export default UserProfile
+export default Welcome;
