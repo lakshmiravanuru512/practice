bb3d5d9e089231e2d5d00fc616f8560a38117ab1
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-15 22:15:15 +0530

    counter example

--- testapp/src/components/index.js
+++ testapp/src/components/index.js
@@ -1,9 +1,28 @@
-import {Component} from "react";
-class Welcome extends Component{
-    render() {
-        const{name}=this.props;
-      return <h1>Hello, {name}</h1>
-    }
+const { Component } = require("react");
+
+
+class Counter extends Component{
+  state={count :0}
+  onIncrease=()=>{
+   this.setState((prevCount)=>({count : prevCount.count+1}))
+  }
+
+  onDecrease=()=>{
+   this.setState((prevCount)=>({count:prevCount.count-1}))
+  }
+  render(){
+   const {count}=this.state
+    return(
+      <div>
+        <h1>Counter</h1>
+        <p>{count}</p>
+        <div>
+          <button onClick={this.onIncrease}>Increase</button>
+          <button onClick={this.onDecrease}>Decrease</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Welcome;
+export default Counter
